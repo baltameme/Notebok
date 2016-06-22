710592c5e6af7499f2b0fd6597ba45689960d22a
Basil Al-Tamiim <basil.altamimi@intigral.net>
2016-06-23 01:28:19 +0400

    Initial commit

---
+++ app/src/main/java/notebook/basil/com/notebok/MainActivityListFragment.java
@@ -0,0 +1,26 @@
+package notebook.basil.com.notebok;
+
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.widget.ListView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainActivityListFragment extends ListFragment {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstance) {
+        super.onActivityCreated(savedInstance);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+    }
+
+
+
+}
---
+++ app/src/main/java/notebook/basil/com/notebok/Note.java
@@ -0,0 +1,7 @@
+package notebook.basil.com.notebok;
+
+/**
+ * Created by apple on 6/23/16.
+ */
+public class Note {
+}
